2e9be950385047e95507bd83ff8d6ef94c752415
MrHappydev <63779197+MrHappydev@users.noreply.github.com>
2020-04-17 15:35:58 +0200

    Game Over Screen dazugeschummelt

---
+++ Assets/GameOver.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    public Transform spieler;
+    public Text over;
+
+    private void Start()
+    {
+        over.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(spieler.position.y < 0)
+        {
+            over.enabled = true;
+        }
+    }
+}
