79ded99fe559e69b112e34dfcc574112444599ec
MrHappydev <63779197+MrHappydev@users.noreply.github.com>
2020-04-17 15:40:13 +0200

    LUL

--- Assets/GameOver.cs
+++ Assets/GameOver.cs
@@ -2,20 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+// ↑ wird benötigt damit public Text angesprochen werden kann
 
 public class GameOver : MonoBehaviour
 {
     public Transform spieler;
     public Text over;
 
+    // warum schnüffelst du eigentlich in meinen Scripts?
     private void Start()
     {
         over.enabled = false;
+        // Am Anfang inaktiv. Deshalb over.enabled = false;
+        //over.disbaled = true gibt's nicht!!!
     }
 
     // Update is called once per frame
     void Update()
-    {
+    { // IF IST KEINE SCHLEIFE!!!!
         if(spieler.position.y < 0)
         {
             over.enabled = true;
