193ace8bb4b1231c3921175e8d2daccf7c16ef3e
MrHappydev <63779197+MrHappydev@users.noreply.github.com>
2020-04-21 12:56:02 +0200

    ...

---
+++ Assets/Scripts/move.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{   //Spitznamen für Rigidbody vergeben. Nicht vergessen die Komponente danach auf den Namen zu ziehen in Unity ;)
+    public Rigidbody rig;
+    public float bewegung = 2000f;
+    public Transform spieler;
+    public int hops = 1;
+    public float sprung = 200f;
+ 
+  
+
+
+   
+    // FixedUpdate immer wenn es um Physik geht die wirken soll!
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+        {
+            rig.AddForce(0, 0, bewegung);
+        }
+        if (Input.GetKey("s"))
+        {
+            rig.AddForce(0, 0, -bewegung);
+        }
+        if (Input.GetKey("d"))
+        {
+            rig.AddForce(bewegung, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rig.AddForce(-bewegung, 0, 0);
+        }
+       
+        
+        
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            if(hops > 0)
+            {
+                rig.AddForce(0, sprung, 0);
+                hops = hops - 1;
+            }
+            
+        }
+    }
+
+    private void OnCollisionEnter(Collision boom)
+    {
+        if(boom.collider.name == "Boden")
+        {
+            hops = 2;
+        }
+        if(boom.collider.tag == "Gegner")
+        {
+            spieler.position = new Vector3(10, 4, 7);
+            rig.velocity = Vector3.zero;
+            rig.angularVelocity = Vector3.zero;
+        }
+    }
+
+
+}
